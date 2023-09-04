8333caf3f7be96830f7b1ae2325c80e0438c1f34
Braedan Kennett <braedan181@gmail.com>
2023-09-04 14:47:58 +1200

    click events added for images, and score label added

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="Myapp">
-    <h1 class="header">Question???</h1>
+    <h1 class="header">Who is most likely to be the most scared while watching a horror movie?</h1>
     <div class="image-container">
-      <div class="image">
-        <!-- Your first image goes here -->
-        <img src="../assets/images/testImage1.jpg" alt="Image 1">
+      <div class="image" @click="handleImageClick(1)">
+        <!-- TODO: Static local images need to be updated -->
+        <h2 class="imageLabel">Image 1</h2>
+        <h2 class="scoreLabel">Score: {{ score }}</h2>
+        <img :src="image1Src" alt="Image 1">
+        
       </div>
-      <div class="image">
-        <!-- Your second image goes here -->
-        <img src="../assets/images/testImage3.jpg" alt="Image 2">
+      <div class="image" @click="handleImageClick(2)">
+        <h2 class="imageLabel">Image 2</h2>
+        <img :src="image2Src" alt="Image 2" >
+        
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import image1Src from '../assets/images/testImage1.jpg';
+import image2Src from '../assets/images/testImage3.jpg';
+
+export default {
+  data() {
+    return {
+      image1Src,
+      image2Src,
+      score: 0,
+    };
+  },
+  methods: {
+    handleImageClick(imageNumber) {
+      // Swap the image sources when Image 1 is clicked
+      if (imageNumber == 1)
+      {
+        const tempSrc = this.image1Src;
+        this.image1Src = this.image2Src;
+        this.image2Src = tempSrc;
+        this.score++;
+      }
+      else{
+
+      }
+      
+    },
+  },
+};
+</script>
 
 <style>
 .Myapp {
@@ -22,6 +57,7 @@
 /* Center the header text vertically and horizontally */
 .header {
   text-align: center;
+  color: white;
   position: absolute; /* Position the header absolutely within the .Myapp container */
   top: 10%; /* Center vertically */
   left: 50%; /* Center horizontally */
@@ -56,7 +92,27 @@
   width: 100%;
   object-fit: cover;
   background-color: transparent;
-  opacity: 0.75;
+  opacity: 0.5;
+}
+
+.imageLabel{
+  color: white;
+  z-index: 1; /* Ensure the header appears in front of the images */
+  position: absolute; /* Position the header absolutely within the .Myapp container */
+  text-align: center;
+  top: 60%; /* Center vertically */
+  left: 50%; /* Center horizontally */
+  transform: translate(-50%, -50%); /*Center the header perfectly*/
+}
+
+.scoreLabel{
+  color: white;
+  z-index: 1; /* Ensure the header appears in front of the images */
+  position: absolute; /* Position the header absolutely within the .Myapp container */
+  text-align: center;
+  top: 97%; /* Center vertically */
+  left: 6%; /* Center horizontally */
+  transform: translate(-50%, -50%); /*Center the header perfectly*/
 }
 
 </style>
