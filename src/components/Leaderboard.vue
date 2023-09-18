15617b65b25c496f466f5717ec3d5a54f238acd4
Tony Nugroho <tonynugroho000@gmail.com>
2023-09-18 13:22:07 +1200

    readded sample code to leaderboard

--- src/components/Leaderboard.vue
+++ src/components/Leaderboard.vue
@@ -19,12 +19,41 @@
 </template>More navigation added and delete account button made.
 
 <script lang="ts">
+import {onMounted} from 'vue'
+import test_top from './test_leaderboard.json'
+
+
+var top;
+var score1 = test_top[0]["Score"]
+var score2 = test_top[1]["Score"]
+var score3 = test_top[2]["Score"]
+var score4 = test_top[3]["Score"]
+var score5 = test_top[4]["Score"]
+
+// fetch("https://unh4y7n697.execute-api.us-east-1.amazonaws.com/prod/leaderboard", {
+//   method: "GET",
+//   headers: {
+//     "Content-Type": "application/json",
+//   }
+// }).then((response) => {
+//   if (response.ok) {
+//     top = response.json();
+//     console.log(top)
+//   }
+// })
+// onMounted(() => {
+  
+// })
 
 export default {
   data() {
       return {
           highScore: 0,
-          score: 0
+          score1: score1,
+          score2: score2,
+          score3: score3,
+          score4: score4,
+          score5: score5
       };
   },
   methods: {
