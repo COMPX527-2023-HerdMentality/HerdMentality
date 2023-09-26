12cf18b1d5c73bbf5fe7c27005cd41402640e7eb
Liam Labuschagne <=>
2023-09-27 00:32:37 +1300

    Add names to leaderboard

--- src/components/Leaderboard.vue
+++ src/components/Leaderboard.vue
@@ -1,380 +1,108 @@
 <template>
-  <div class="Myapp_leaderboard">
-    <h1 class="header_leaderboard">Leaderboard</h1>
-    <h2 class="score1_leaderboard">1. {{ score1 }}</h2>
-    <h2 class="score2_leaderboard">2. {{ score2 }}</h2>
-    <h2 class="score3_leaderboard">3. {{ score3 }}</h2>
-    <h2 class="score4_leaderboard">4. {{ score4 }}</h2>
-    <h2 class="score5_leaderboard">5. {{ score5 }}</h2>
-    <h2 class="highScore_leaderboard">Your Highscore: {{ highScore }}</h2>
-    <router-link to="/play"><input id="play_leaderboard" type="button" value="Play" /></router-link>
-    <router-link to="/"><input id="home_leaderboard" type="button" value="Home" /></router-link>
-    <div class="image-container_leaderboard">
-      <div class="image_leaderboard">
-        <!-- Your first image goes here -->
-        <img src="../assets/images/leaderboard.jpg" alt="Image 1" />
-      </div>
+  <div id="leaderboard-container">
+    <div id="leaderboard">
+      <h1 class="title">Leaderboard</h1>
+      <p class="leaderboard-item" v-for="score in scores">{{score.username}}: {{ score.score }}</p>
+      <router-link class="button" to="/play"><button>Play again</button></router-link>
+      <router-link class="button" to="/"><button>Back</button></router-link>
+    </div>
+    <div id="background-image-container">
+      <div id="background-image"></div>
     </div>
   </div>
 </template>
-More navigation added and delete account button made.
 
 <script lang="ts" setup>
-import { ref } from 'vue'
-import test_top from './test_leaderboard.json'
+import { reactive } from 'vue'
 
-var score1 = ref(0)
-var score2 = ref(0)
-var score3 = ref(0)
-var score4 = ref(0)
-var score5 = ref(0)
+const arr:any = [];
+
+
+let scores = reactive(arr)
+
 var highScore = 0
+
+
 
 fetch('https://rvunpy4go9.execute-api.us-east-1.amazonaws.com/prod/leaderboard').then((res) => {
   res.json().then((data) => {
     console.log(data)
-    score1.value = data[0]['Score']
-    score2.value = data[1]['Score']
-    score3.value = data[2]['Score']
-    score4.value = data[3]['Score']
-    score5.value = data[4]['Score']
+    data.forEach((item:any) => {
+      scores.push({score: item.Score, username: item.Username})
+    })
   })
 })
 </script>
 
-<style>
+<style scoped>
 @font-face {
   font-family: sheepFont;
-  src: url(../assets/fonts/sheep.ttf);
+  src: url('../assets/fonts/sheep.ttf');
 }
 
-.Myapp_leaderboard {
-  width: 100%;
+* {
+  font-family: sheepFont
 }
 
-/* Center the header text vertically and horizontally */
-.header_leaderboard {
-  position: absolute; /* Position the header absolutely within the .Myapp container */
-  top: 13%; /* Center vertically */
-
-  left: 50%; /* Center horizontally */
-  transform: translate(-50%, -50%); /*Center the header perfectly*/
-  z-index: 1; /* Ensure the header appears in front of the images */
-  font-family: sheepFont;
-  font-size: 110pt;
-  color: #cc998d;
+#title {
+  display:flex;
 }
 
-.score1_leaderboard {
-  position: absolute; /* Position the header absolutely within the .Myapp container */
-  top: 30%; /* Center vertically */
-  left: 50%; /* Center horizontally */
-  transform: translate(-50%, -50%); /*Center the header perfectly*/
-  z-index: 1; /* Ensure the header appears in front of the images */
-  font-family: sheepFont;
-  font-size: 90pt;
-  color: #a6cbce;
+button {
+  display:block;
+  margin: auto;
+  margin-top: 2rem;
+  background: none;
+  color:#c3c2bf;
+  border:none;
 }
 
-.score2_leaderboard {
-  position: absolute; /* Position the header absolutely within the .Myapp container */
-  top: 40%; /* Center vertically */
-  left: 50%; /* Center horizontally */
-  transform: translate(-50%, -50%); /*Center the header perfectly*/
-  z-index: 1; /* Ensure the header appears in front of the images */
-  font-family: sheepFont;
-  font-size: 75pt;
-  color: #a6cbce;
+button:hover {
+  cursor: pointer;
 }
 
-.score3_leaderboard {
-  position: absolute; /* Position the header absolutely within the .Myapp container */
-  top: 49%; /* Center vertically */
-  left: 50%; /* Center horizontally */
-  transform: translate(-50%, -50%); /*Center the header perfectly*/
-  z-index: 1; /* Ensure the header appears in front of the images */
-  font-family: sheepFont;
-  font-size: 60pt;
-  color: #a6cbce;
+#leaderboard {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  z-index: 100;
+  text-align: center;
+  width: 100vw;
 }
 
-.score4_leaderboard {
-  position: absolute; /* Position the header absolutely within the .Myapp container */
-  top: 57%; /* Center vertically */
-  left: 50%; /* Center horizontally */
-  transform: translate(-50%, -50%); /*Center the header perfectly*/
-  z-index: 1; /* Ensure the header appears in front of the images */
-  font-family: sheepFont;
-  font-size: 50pt;
-  color: #a6cbce;
+#background-image-container {
+  position: absolute;
+  z-index: 0;
+  top: 0px;
+  left: 0px;
+  background-color: black;
 }
 
-.score5_leaderboard {
-  position: absolute; /* Position the header absolutely within the .Myapp container */
-  top: 64%; /* Center vertically */
-  left: 50%; /* Center horizontally */
-  transform: translate(-50%, -50%); /*Center the header perfectly*/
-  z-index: 1; /* Ensure the header appears in front of the images */
-  font-family: sheepFont;
-  font-size: 50pt;
-  color: #a6cbce;
-}
-
-.highScore_leaderboard {
-  position: absolute; /* Position the header absolutely within the .Myapp container */
-  top: 76%; /* Center vertically */
-  left: 50%; /* Center horizontally */
-  transform: translate(-50%, -50%); /*Center the header perfectly*/
-  z-index: 1; /* Ensure the header appears in front of the images */
-  font-family: sheepFont;
-  font-size: 50pt;
-  color: #cc998d;
-}
-
-#play_leaderboard {
-  position: absolute; /* Position the header absolutely within the .Myapp container */
-  top: 90%; /* Center vertically */
-  left: 30%; /* Center horizontally */
-  transform: translate(-50%, -50%); /*Center the header perfectly*/
-  z-index: 1; /* Ensure the header appears in front of the images */
-  font-family: sheepFont;
-  font-size: 70pt;
-  background-color: transparent;
-  border: none;
-  color: #cc998d;
-}
-
-#play_leaderboard:hover {
-  font-size: 80pt;
-  color: #dc816c;
-}
-
-#home_leaderboard {
-  position: absolute; /* Position the header absolutely within the .Myapp container */
-  top: 90%; /* Center vertically */
-  left: 70%; /* Center horizontally */
-  transform: translate(-50%, -50%); /*Center the header perfectly*/
-  z-index: 1; /* Ensure the header appears in front of the images */
-  font-family: sheepFont;
-  font-size: 70pt;
-  background-color: transparent;
-  border: none;
-  color: #cc998d;
-}
-
-#home_leaderboard:hover {
-  font-size: 80pt;
-  color: #dc816c;
-}
-
-#leaderboards {
-  position: absolute; /* Position the header absolutely within the .Myapp container */
-  top: 82%; /* Center vertically */
-  left: 50%; /* Center horizontally */
-  transform: translate(-50%, -50%); /*Center the header perfectly*/
-  z-index: 1; /* Ensure the header appears in front of the images */
-  font-family: sheepFont;
-  font-size: 50pt;
-  background-color: transparent;
-  border: none;
-  color: #cc998d;
-}
-
-#leaderboards:hover {
-  font-size: 60pt;
-  color: #dc816c;
-}
-
-/* Create a flex container to hold the images side by side */
-.image-container_leaderboard {
-  display: flex;
-  justify-content: space-between;
+#background-image {
+  width: 100vw;
   height: 100vh;
-  width: 200%;
-  background-color: rgba(0, 0, 0, 1);
-}
-
-/* Style for each image */
-.image_leaderboard {
-  flex: 1; /*Each image takes up an equal part of the container*/
-  text-align: center; /* Center the content horizontally */
-  /* background-size: cover; Scale the background image to cover the entire div */
-  background-position: 0%; /* Center the background image within the div */
-  overflow: hidden;
-  position: relative;
-}
-
-.image_leaderboard img {
-  height: auto;
-  width: 100%;
-  object-fit: cover;
-  background-color: transparent;
+  background-image: url(../assets/images/leaderboard.jpg);
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size:cover;
+  background-position: center;
   opacity: 0.5;
 }
 
-@media (max-width: 768px) {
-  .Myapp_leaderboard {
-    width: 100%;
-  }
-
-  /* Center the header text vertically and horizontally */
-  .header_leaderboard {
-    position: absolute; /* Position the header absolutely within the .Myapp container */
-    top: 13%; /* Center vertically */
-
-    left: 50%; /* Center horizontally */
-    transform: translate(-50%, -50%); /*Center the header perfectly*/
-    z-index: 1; /* Ensure the header appears in front of the images */
-    font-family: sheepFont;
-    font-size: 50pt;
-    color: #cc998d;
-  }
-
-  .score1_leaderboard {
-    position: absolute; /* Position the header absolutely within the .Myapp container */
-    top: 22%; /* Center vertically */
-    left: 50%; /* Center horizontally */
-    transform: translate(-50%, -50%); /*Center the header perfectly*/
-    z-index: 1; /* Ensure the header appears in front of the images */
-    font-family: sheepFont;
-    font-size: 45pt;
-    color: #a6cbce;
-  }
-
-  .score2_leaderboard {
-    position: absolute; /* Position the header absolutely within the .Myapp container */
-    top: 31%; /* Center vertically */
-    left: 50%; /* Center horizontally */
-    transform: translate(-50%, -50%); /*Center the header perfectly*/
-    z-index: 1; /* Ensure the header appears in front of the images */
-    font-family: sheepFont;
-    font-size: 40pt;
-    color: #a6cbce;
-  }
-
-  .score3_leaderboard {
-    position: absolute; /* Position the header absolutely within the .Myapp container */
-    top: 40%; /* Center vertically */
-    left: 50%; /* Center horizontally */
-    transform: translate(-50%, -50%); /*Center the header perfectly*/
-    z-index: 1; /* Ensure the header appears in front of the images */
-    font-family: sheepFont;
-    font-size: 35pt;
-    color: #a6cbce;
-  }
-
-  .score4_leaderboard {
-    position: absolute; /* Position the header absolutely within the .Myapp container */
-    top: 48%; /* Center vertically */
-    left: 50%; /* Center horizontally */
-    transform: translate(-50%, -50%); /*Center the header perfectly*/
-    z-index: 1; /* Ensure the header appears in front of the images */
-    font-family: sheepFont;
-    font-size: 30pt;
-    color: #a6cbce;
-  }
-
-  .score5_leaderboard {
-    position: absolute; /* Position the header absolutely within the .Myapp container */
-    top: 56%; /* Center vertically */
-    left: 50%; /* Center horizontally */
-    transform: translate(-50%, -50%); /*Center the header perfectly*/
-    z-index: 1; /* Ensure the header appears in front of the images */
-    font-family: sheepFont;
-    font-size: 25pt;
-    color: #a6cbce;
-  }
-
-  .highScore_leaderboard {
-    position: absolute; /* Position the header absolutely within the .Myapp container */
-    top: 70%; /* Center vertically */
-    left: 50%; /* Center horizontally */
-    transform: translate(-50%, -50%); /*Center the header perfectly*/
-    z-index: 1; /* Ensure the header appears in front of the images */
-    font-family: sheepFont;
-    font-size: 25pt;
-    color: #cc998d;
-  }
-
-  #play_leaderboard {
-    position: absolute; /* Position the header absolutely within the .Myapp container */
-    top: 85%; /* Center vertically */
-    left: 30%; /* Center horizontally */
-    transform: translate(-50%, -50%); /*Center the header perfectly*/
-    z-index: 1; /* Ensure the header appears in front of the images */
-    font-family: sheepFont;
-    font-size: 50pt;
-    background-color: transparent;
-    border: none;
-    color: #cc998d;
-  }
-
-  #play_leaderboard:hover {
-    font-size: 60pt;
-    color: #dc816c;
-  }
-
-  #home_leaderboard {
-    position: absolute; /* Position the header absolutely within the .Myapp container */
-    top: 85%; /* Center vertically */
-    left: 70%; /* Center horizontally */
-    transform: translate(-50%, -50%); /*Center the header perfectly*/
-    z-index: 1; /* Ensure the header appears in front of the images */
-    font-family: sheepFont;
-    font-size: 50pt;
-    background-color: transparent;
-    border: none;
-    color: #cc998d;
-  }
-
-  #home_leaderboard:hover {
-    font-size: 60pt;
-    color: #dc816c;
-  }
-
-  #leaderboards {
-    position: absolute; /* Position the header absolutely within the .Myapp container */
-    top: 82%; /* Center vertically */
-    left: 50%; /* Center horizontally */
-    transform: translate(-50%, -50%); /*Center the header perfectly*/
-    z-index: 1; /* Ensure the header appears in front of the images */
-    font-family: sheepFont;
-    font-size: 50pt;
-    background-color: transparent;
-    border: none;
-    color: #cc998d;
-  }
-
-  #leaderboards:hover {
-    font-size: 60pt;
-    color: #dc816c;
-  }
-
-  /* Create a flex container to hold the images side by side */
-  .image-container_leaderboard {
-    display: flex;
-    justify-content: space-between;
-    height: 100vh;
-    width: 200%;
-    background-color: rgba(0, 0, 0, 1);
-  }
-
-  /* Style for each image */
-  .image_leaderboard {
-    flex: 1; /*Each image takes up an equal part of the container*/
-    text-align: center; /* Center the content horizontally */
-    /* background-size: cover; Scale the background image to cover the entire div */
-    background-position: 0%; /* Center the background image within the div */
-    overflow: hidden;
-    position: relative;
-  }
-
-  .image_leaderboard img {
-    height: 100%;
-    width: 100%;
-    object-fit: cover;
-    background-color: transparent;
-    opacity: 0.5;
-  }
+#background-image-container {
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  top: 0px;
+  left: 0px;
 }
+
+h1 {
+  font-size: 10em;
+}
+
+p, button {
+  font-size: 5em;
+}
+
 </style>
